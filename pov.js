492e9f9f7bf5b2134e0c06b3571cee11c08e47fa
SIVASUBRAMANI M <sivasubramanim.23mts@kongu.edu>
2025-06-19 16:14:38 +0530

    second

---
+++ pov.js
@@ -0,0 +1,33 @@
+/*let a=0;
+//let e= "siva ";//string
+//let isbool = true; //booleon
+//let c; //undifined
+//let b= null;//null
+//let abc=23456789;//bigint
+//console.log(e);
+//console.log(b);
+//console.log(isbool);
+//operators
+//arithematic +-*/
+//comprison == >< <== === != !== 
+// logic opoerator &&||!|
+let a=0;
+let b=20;
+//console.log(a+b);
+/////console.log(a-b);
+//console.log(a*b);
+//console.log(a/b);
+//console.log(a==b,a<b,a>b,a===b,a||b,a&&b);
+
+
+if(a<b){
+    console.log("yess")
+}
+else if(a==0){
+    console.log("0")
+}
+else{
+    console.log("No")
+}
+
+
